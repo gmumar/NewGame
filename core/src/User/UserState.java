9d7b20bf4d7560a2063cc3597c5da19226431248
Umar <gmumar@gmail.com>
2016-04-10 23:35:51 -0400

    bug fixes with editor loading premade cars

---
+++ core/src/User/UserState.java
@@ -0,0 +1,11 @@
+package User;
+
+public class UserState {
+
+	public Integer smallBarLevel = 1;
+	public Integer tireLevel = 1;
+	public Integer springLevel = 1;
+	public Integer money = 10000;
+	
+	
+}
